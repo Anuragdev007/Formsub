57a6c060eee3c3dba410ebf92bd82c0996b270fa
Anurag <anuragbhardwaj1333@gmail.com>
2020-10-04 09:55:42 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+
+dbPassword='mongodb+srv://Anurag-007:'+encodeURIComponent('Anurag@181011')+'@cluster0.4g7xr.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+module.exports={
+    mongoURI: dbPassword
+};
+
+
+
